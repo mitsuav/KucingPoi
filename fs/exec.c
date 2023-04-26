27a0a9e59c702190d5f481eba1e435dee311407b
Le Hong Duc <hongducthbk123@gmail.com>
2023-04-26 17:47:00 +0000

    Implement KernelSU Signed-off-by: nadinsylaa <asepmlnaaa@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1716,6 +1716,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+// KernelSU hook
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1729,6 +1735,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
